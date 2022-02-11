a1ba98d5abea5b1c39834157a64ff2820887998c
winterminer <99415502+winterminer@users.noreply.github.com>
2022-02-11 11:25:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ btn.addEventListener("click", function() {
     window.location.href = url;
 });
 
-btn.innerHTML = "Join t.me/" + url.replace("tg://resolve?domain=", "");
+btn.innerHTML = "t.me/teenminers" + url.replace("tg://resolve?domain=", "");
